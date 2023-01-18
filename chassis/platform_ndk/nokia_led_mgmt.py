e49ddfb0b5c2e2adefdb2e65909ffb0eced387a9
mlok <marty.lok@nokia.com>
2023-01-18 17:49:38 -0500

    Fix the LedContorlCommon to get the physical index from port mapping

--- chassis/platform_ndk/nokia_led_mgmt.py
+++ chassis/platform_ndk/nokia_led_mgmt.py
@@ -9,7 +9,9 @@
 from platform_ndk import nokia_common
 from platform_ndk import platform_ndk_pb2
 from sonic_py_common.logger import Logger
+from sonic_py_common import multi_asic, device_info
 from sonic_platform_base.device_base import DeviceBase
+from sonic_platform_base.sonic_sfp.sfputilhelper import SfpUtilHelper
 try:
     from sonic_led import led_control_base
 except ImportError as e:
@@ -21,6 +23,18 @@ logger = Logger(SYSLOG_IDENTIFIER)
 
 class LedControlCommon(led_control_base.LedControlBase):
     def __init__(self):
+        self.platform_sfputil = SfpUtilHelper()
+        if multi_asic.is_multi_asic():
+            # For multi ASIC platforms we pass DIR of port_config_file_path and the number of asics
+            (platform_path, hwsku_path) = device_info.get_paths_to_platform_and_hwsku_dirs()
+
+            # Load platform module from source
+            self.platform_sfputil.read_all_porttab_mappings(hwsku_path, multi_asic.get_num_asics())
+        else:
+            # For single ASIC platforms we pass port_config_file_path and the asic_inst as 0
+            port_config_file_path = device_info.get_path_to_port_config_file()
+            self.platform_sfputil.read_porttab_mappings(port_config_file_path, 0)
+
         led_type = platform_ndk_pb2.ReqLedType.LED_TYPE_PORT
         led_info = nokia_common.led_color_to_info(DeviceBase.STATUS_LED_COLOR_OFF)
         channel, stub = nokia_common.channel_setup(nokia_common.NOKIA_GRPC_LED_SERVICE)
@@ -39,9 +53,11 @@ class LedControlCommon(led_control_base.LedControlBase):
         if port.startswith(intf_prefix) is False:
             return
         else:
-            import re
-            parse_nums = re.findall(r'\d+', port)
-            port_id = int(parse_nums[0])
+            if self.platform_sfputil.is_logical_port(port):
+                port_id_list = self.platform_sfputil.get_logical_to_physical(port)
+                port_id = port_id_list[0] - 1
+            else:
+                return
 
         if state == 'up':
             port_up = 1
