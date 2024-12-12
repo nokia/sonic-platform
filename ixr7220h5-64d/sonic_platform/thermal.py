89b784e228e5e37ed0abc7c99243b99c6b548bb2
y7zhou <yi.1.zhou@nokia.com>
2024-12-12 09:44:39 -0500

    [H5-64D]Add ports_notify and update FW upgrade, thermal info and watcdog modules

--- ixr7220h5-64d/sonic_platform/thermal.py
+++ ixr7220h5-64d/sonic_platform/thermal.py
@@ -1,48 +1,38 @@
-########################################################################
-# Nokia IXR7220-H5-64D
-#
-# Module contains an implementation of SONiC Platform Base API and
-# provides the Thermals' information which are available in the platform
-#
-########################################################################
+"""
+    Nokia IXR7220-H5-64D
 
+    Module contains an implementation of SONiC Platform Base API and
+    provides the Thermals' information which are available in the platform
+"""
 
 try:
-    import os
     import glob
     from sonic_platform_base.thermal_base import ThermalBase
     from sonic_py_common import logger
-    from sonic_py_common import multi_asic
-    from swsscommon import swsscommon
-    from swsscommon.swsscommon import SonicV2Connector,ConfigDBConnector
+    from swsscommon.swsscommon import SonicV2Connector
+    from sonic_platform.sysfs import read_sysfs_file
 except ImportError as e:
-    raise ImportError(str(e) + "- required module not found")
+    raise ImportError(str(e) + ' - required module not found') from e
 
 sonic_logger = logger.Logger('thermal')
 
-H5_64D_THERMAL = 10
+THERMAL_NUM = 11
 
 class Thermal(ThermalBase):
     """Nokia platform-specific Thermal class"""
 
-    HWMON_DIR = "/sys/bus/i2c/devices/{}/hwmon/hwmon*/"    
-    I2C_DEV_LIST = ["6-004f", "6-004b", "6-004a", "6-004e", "6-0049", 
-                    "5-0049", "6-0048", "7-004f", "7-004e"]
-    THERMAL_NAME = ["MB Left", 
-                    "MB Front", 
-                    "MB Right", 
-                    "MB MAC", 
-                    "PSU top", 
-                    "PSU Bottom", 
-                    "CPU Board", 
-                    "Fan Left", 
-                    "Fan Right", 
-                    "ASIC TH5"]
+    HWMON_DIR = "/sys/bus/i2c/devices/{}/hwmon/hwmon*/"
+    I2C_DEV_LIST = ["20-0048", "16-004b", "16-0049", "16-004a", "16-004e", 
+                    "16-004f", "17-004e", "17-004f", "19-0049"]
+    THERMAL_NAME = ["CPU Board", "MB Front", "PSU Top", "MB Right", "MB MAC", 
+                    "MB Left", "Fan Right", "Fan Left", "PSU Bottom", "ASIC TH5", "CPU"]
+    THRESHHOLD = [72.0, 60.0, 60.0, 58.0, 82.0, 63.0, 59.0, 60.0, 66.0, 92.0, 95.0]
+    CRITICAL_THRESHHOLD = [75.0, 63.0, 63.0, 61.0, 85.0, 66.0, 62.0, 63.0, 69.0, 95.0, 100.0]
 
     def __init__(self, thermal_index):
         ThermalBase.__init__(self)
-        self.index = thermal_index + 1        
-        if self.index == 6:
+        self.index = thermal_index + 1
+        if self.index == 8 or self.index == 9:
             self.is_fan_thermal = True
         else:
             self.is_fan_thermal = False
@@ -50,35 +40,18 @@ class Thermal(ThermalBase):
         self._minimum = None
         self._maximum = None
         self.thermal_high_threshold_file = None
-        
+
         # sysfs file for crit high threshold value if supported for this sensor
         self.thermal_high_crit_threshold_file = None
 
-        if self.index == H5_64D_THERMAL:    # MAC internal sensor
-            self.thermal_temperature_file = None            
+        if self.index == THERMAL_NUM:    # CPU
+            self.device_path = glob.glob("/sys/devices/pci0000:00/0000:00:18.3/hwmon/hwmon*/")
+            self.thermal_temperature_file = self.device_path[0] + "temp1_input"
+        elif self.index == THERMAL_NUM-1:    # MAC internal sensor
+            self.thermal_temperature_file = None
         else:
-            self.device_path = glob.glob(self.HWMON_DIR.format(self.I2C_DEV_LIST[self.index - 1]))        
-
-            # sysfs file for current temperature value
-            self.thermal_temperature_file = self.device_path[0] + "temp1_input"                 
-
-    def _read_sysfs_file(self, sysfs_file):
-        # On successful read, returns the value read from given
-        # sysfs_file and on failure returns 'ERR'
-        rv = 'ERR'
-
-        if (not os.path.isfile(sysfs_file)):
-            return rv
-
-        try:
-            with open(sysfs_file, 'r') as fd:
-                rv = fd.read()
-        except Exception as e:
-            rv = 'ERR'
-
-        rv = rv.rstrip('\r\n')
-        rv = rv.lstrip(" ")
-        return rv
+            self.device_path = glob.glob(self.HWMON_DIR.format(self.I2C_DEV_LIST[self.index - 1]))
+            self.thermal_temperature_file = self.device_path[0] + "temp1_input"
 
     def get_name(self):
         """
@@ -98,8 +71,7 @@ class Thermal(ThermalBase):
         """
         if self.dependency:
             return self.dependency.get_presence()
-        else:
-            return True
+        return True
 
     def get_model(self):
         """
@@ -129,8 +101,7 @@ class Thermal(ThermalBase):
         """
         if self.dependency:
             return self.dependency.get_status()
-        else:
-            return True
+        return True
 
     def get_temperature(self):
         """
@@ -140,23 +111,24 @@ class Thermal(ThermalBase):
             A float number of current temperature in Celsius up to
             nearest thousandth of one degree Celsius, e.g. 30.125
         """
-        if self.index == H5_64D_THERMAL:
+        if self.index == THERMAL_NUM-1:
             db = SonicV2Connector()
             db.connect(db.STATE_DB)
             data_dict = db.get_all(db.STATE_DB, 'ASIC_TEMPERATURE_INFO')
             thermal_temperature = float(data_dict['maximum_temperature'])
         else:
-            thermal_temperature = self._read_sysfs_file(self.thermal_temperature_file)
+            thermal_temperature = read_sysfs_file(self.thermal_temperature_file)
             if (thermal_temperature != 'ERR'):
                 thermal_temperature = float(thermal_temperature) / 1000
-                if self._minimum is None or self._minimum > thermal_temperature:
-                    self._minimum = thermal_temperature
-                if self._maximum is None or self._maximum < thermal_temperature:
-                    self._maximum = thermal_temperature
             else:
                 thermal_temperature = 0
+        
+        if self._minimum is None or self._minimum > thermal_temperature:
+            self._minimum = thermal_temperature
+        if self._maximum is None or self._maximum < thermal_temperature:
+            self._maximum = thermal_temperature
 
-        return float("{:.3f}".format(thermal_temperature))
+        return float(f"{thermal_temperature:.3f}")
 
     def get_high_threshold(self):
         """
@@ -167,10 +139,7 @@ class Thermal(ThermalBase):
             Celsius up to nearest thousandth of one degree Celsius,
             e.g. 30.125
         """
-        if self.index == H5_64D_THERMAL:
-            return 100.0
-        else:        
-            return 78.0  
+        return self.THRESHHOLD[self.index - 1]
 
     def set_high_threshold(self, temperature):
         """
@@ -194,11 +163,7 @@ class Thermal(ThermalBase):
             A float number, the high critical threshold temperature of thermal in Celsius
             up to nearest thousandth of one degree Celsius, e.g. 30.125
         """
-
-        if self.index == H5_64D_THERMAL:
-            return 103.0
-        else:        
-            return 80.0 
+        return self.CRITICAL_THRESHHOLD[self.index - 1]
 
     def set_high_critical_threshold(self):
         """
@@ -222,7 +187,7 @@ class Thermal(ThermalBase):
             up to nearest thousandth of one degree Celsius, e.g. 30.125
         """
         return 0.0
-    
+
     def set_low_threshold(self, temperature):
         """
         Sets the low threshold temperature of thermal
@@ -236,12 +201,18 @@ class Thermal(ThermalBase):
         """
         # Thermal threshold values are pre-defined based on HW.
         return False
-    
+
     def get_minimum_recorded(self):
+        """
+        Retrieves minimum recorded temperature
+        """
         self.get_temperature()
         return self._minimum
 
     def get_maximum_recorded(self):
+        """
+        Retrieves maxmum recorded temperature
+        """
         self.get_temperature()
         return self._maximum
 
